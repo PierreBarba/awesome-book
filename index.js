a72621c50a1b9b23f4f6e806ea389e038d9fdee7
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-12-06 16:43:25 -0600

    linter fix css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 class BookCollection {
   #index;
+
   tasks;
 
   getTasks() {
@@ -9,7 +10,7 @@ class BookCollection {
     for (let i = 0; i < this.tasks.length; i += 1) {
       const { title } = this.tasks[i];
       const { author } = this.tasks[i];
-  
+
       tasksView.innerHTML += `
       <div class="book-card" id = "${title}">
           <p class="book-title">${title}</p>
@@ -20,14 +21,15 @@ class BookCollection {
       </div>`;
     }
   }
-  saveTask () {
+
+  saveTask() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const task = {
       title,
       author,
     };
-  
+
     if (localStorage.getItem('tasks') === null) {
       const tasks = [];
       tasks.push(task);
@@ -37,11 +39,11 @@ class BookCollection {
       tasks.push(task);
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
-  
+
     this.getTasks();
     document.getElementById('form').reset();
   }
-  
+
   deleteTask(title) {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     for (let i = 0; i < tasks.length; i += 1) {
@@ -51,13 +53,12 @@ class BookCollection {
     }
     document.getElementById(title).remove();
     localStorage.setItem('tasks', JSON.stringify(tasks));
-   
   }
 }
 
-let books = new BookCollection();
-books.getTasks()
-document.getElementById('form').addEventListener('submit', (e) =>{
+const books = new BookCollection();
+books.getTasks();
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  books.saveTask()
+  books.saveTask();
 });
