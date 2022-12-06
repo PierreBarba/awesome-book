80975b6952d6e19a62d54f40b4dd065a7e954fd7
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-12-06 15:48:02 -0600

    change classes

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-function getTasks() {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const tasksView = document.getElementById('tasks');
-  tasksView.innerHTML = '';
-  for (let i = 0; i < tasks.length; i += 1) {
-    const { title } = tasks[i];
-    const { author } = tasks[i];
-
-    tasksView.innerHTML += `
-      <div class="book-card">
+class BookCollection {
+  #index;
+  tasks;
+ 
+  getTasks() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const tasksView = document.getElementById('tasks');
+    tasksView.innerHTML = '';
+    for (let i = 0; i < this.tasks.length; i += 1) {
+      const { title } = this.tasks[i];
+      const { author } = this.tasks[i];
+  
+      tasksView.innerHTML += `
+      <div class="book-card" id = "${title}">
           <p class="book-title">${title}</p>
           <p class="book-author">${author}</p>
-          <button type="button" class="book-btn" onclick="deleteTask('${title}')">Remove</button>
-      </div>
-      <hr>`;
-  }
-}
-getTasks();
-
-function saveTask(e) {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-
-  const task = {
-    title,
-    author,
-  };
-
-  if (localStorage.getItem('tasks') === null) {
-    const tasks = [];
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  } else {
-    const tasks = JSON.parse(localStorage.getItem('tasks'));
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-
-  getTasks();
-  document.getElementById('form').reset();
-  e.preventDefault();
-}
-
-function deleteTask(title) {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  for (let i = 0; i < tasks.length; i += 1) {
-    if (tasks[i].title === title) {
-      tasks.splice(i, 1);
+          <button type="button" class="book-btn" onclick="books.deleteTask('${title}')">Remove</button>
+          <hr>
+      </div>`;
     }
   }
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  getTasks();
+  saveTask () {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const task = {
+      title,
+      author,
+    };
+  
+    if (localStorage.getItem('tasks') === null) {
+      const tasks = [];
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  
+    this.getTasks();
+    document.getElementById('form').reset();
+  }
+  
+  deleteTask(title) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    for (let i = 0; i < tasks.length; i += 1) {
+      if (tasks[i].title === title) {
+        tasks.splice(i, 1);
+      }
+    }
+    document.getElementById(title).remove();
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+   
+  }
 }
-deleteTask();
 
-document.getElementById('form').addEventListener('submit', saveTask);
+let books = new BookCollection();
+books.getTasks()
+document.getElementById('form').addEventListener('submit', (e) =>{
+  e.preventDefault();
+  books.saveTask()
+});
